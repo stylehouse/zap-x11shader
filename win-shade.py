f0d2badc3c8b9a327aefd0a8f65437ead3828caa
Steve <ostylehouse@gmail.com>
2023-07-13 02:39:05 +1200

    give up on py xlib

--- win-shade.py
+++ win-shade.py
@@ -1,50 +1,76 @@
 #!/usr/bin/env python3
 import Xlib.display
 import numpy as np
-import cv2
-import re
-import pprint
-def dd(data,depth=7):
-    pp = pprint.PrettyPrinter(depth=depth)
-    pp.pprint(data)
+from PIL import Image
 
-# Alters the pixels directly on the screen for an existing X window#
-#  blending it with an image via multiply.
-def alter_window_pixels(display, window, image):
-    win = window.get_geometry()
-    image_width, image_height = image.shape[:2]
+image_path = "boreo_desert.jpg"
 
-    filtered_image = np.zeros((win.width, win.height, 3), dtype=np.uint8)
-    raw_data = window.get_image(0, 0, win.width, win.height, Xlib.X.ZPixmap, 0xffffffff).data
-    for x in range(win.width):
-        for y in range(win.height):
-            pixel_offset = (y * win.width + x) * 4
-            pixel = raw_data[pixel_offset:pixel_offset + 3][::-1]  # BGR to RGB
-            filtered_image[x, y] = np.multiply(pixel, image[x % image_width, y % image_height])
+def create_window_with_image():
+    # Connect to the X server
+    display = Xlib.display.Display()
 
-    window.put_image(0, 0, filtered_image.flatten().tobytes(), win.width, win.height, Xlib.X.ZPixmap, 32)
+    # Create a new window
+    screen = display.screen()
+    window = screen.root.create_window(
+        0, 0, 800, 600, 0, screen.root_depth, Xlib.X.InputOutput, Xlib.X.CopyFromParent,
+        background_pixel=screen.white_pixel, event_mask=Xlib.X.ExposureMask | Xlib.X.ButtonPressMask
+    )
+    window.map()
 
-def find_nicotine_window(d,root):
-    window_ids = root.get_full_property(d.intern_atom('_NET_CLIENT_LIST'), Xlib.X.AnyPropertyType).value
-    nicotine_window = None
-    for window_id in window_ids:
-        window = d.create_resource_object('window', window_id)
-        wm_name = window.get_wm_name()
-        dd(wm_name)
-        if re.search(r'^Nicotine\+',wm_name):
-            nicotine_window = window
+    # Get the window's visual
+    visual = screen.root_visual
+
+    # Load and resize the image
+    image = Image.open(image_path)
+    image = image.resize((800, 600))
+
+    # Convert the image to RGBA mode
+    image = image.convert("RGBA")
+
+    # Create an XImage from the image data
+    image_data = np.array(image)
+    image_data_bytes = image_data.tobytes()
+    image_width, image_height = image.size
+    ximage = Xlib.ext.image_to_pil.ImageWrapper(
+        image_width, image_height, image_data_bytes,
+        format='rgba', bits_per_pixel=32, byte_order=display.byte_order,
+        bitmap_unit=32, bitmap_bit_order=display.byte_order
+    )
+
+    # Set the window attributes for transparency
+    window.change_attributes(backing_store=Xlib.X.NotUseful, save_under=False)
+    window.change_property(
+        display.intern_atom('_NET_WM_WINDOW_OPACITY'), Xlib.X.XA_CARDINAL, 32, [0xFFFFFFFF]
+    )
+
+    # Set the compositing hint to enable blending
+    window.change_property(
+        display.intern_atom('_NET_WM_WINDOW_TYPE'), Xlib.X.XA_ATOM, 32,
+        [display.intern_atom('_NET_WM_WINDOW_TYPE_NORMAL')]
+    )
+
+    # Set the visual for the window
+    window.change_attributes(visual=visual)
+
+    # Create a graphics context for drawing
+    gc = window.create_gc(
+        foreground=screen.black_pixel, background=screen.white_pixel,
+        graphics_exposures=False, line_width=1
+    )
+
+    # Draw the blended image on the window
+    window.put_image(gc, 0, 0, ximage)
+
+    # Flush the changes to the server
+    display.flush()
+
+    # Enter the event loop
+    while True:
+        event = display.next_event()
+        if event.type == Xlib.X.ButtonPress:
             break
-    if not nicotine_window:
-       exit(8)
-    return nicotine_window
 
-def doit():
-    d = Xlib.display.Display()
-    root = d.screen().root
-    window = find_nicotine_window(d,root)
-    image = cv2.imread("boreo_desert.jpg")
+    # Clean up and close the display connection
+    display.close()
 
-    alter_window_pixels(d, window, image)
-
-if __name__ == "__main__":
-    doit()
+create_window_with_image()
